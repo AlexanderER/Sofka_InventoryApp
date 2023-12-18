06c8dab3c34670011952566983a2112c30343e25
AlexanderER <alexer1515@gmail.com>
2023-12-18 12:39:07 -0600

    Initial commit

---
+++ src/test/java/epa/InventoryApp/InventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package epa.InventoryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
