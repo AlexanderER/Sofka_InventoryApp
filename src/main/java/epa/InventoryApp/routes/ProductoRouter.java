808b467a103722af714544e483bde0bb6b8e9c53
AlexanderER <alexer1515@gmail.com>
2023-12-18 23:38:59 -0600

    Implementación de useCase para listar inventario Paginado

--- src/main/java/epa/InventoryApp/routes/ProductoRouter.java
+++ src/main/java/epa/InventoryApp/routes/ProductoRouter.java
@@ -2,14 +2,19 @@ package epa.InventoryApp.routes;
 
 import epa.InventoryApp.models.dto.ProductoDTO;
 import epa.InventoryApp.usecase.producto.CrearProductoUseCase;
+import epa.InventoryApp.usecase.producto.ListarProductosPaginadoUseCaseImp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import java.math.BigDecimal;
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -29,5 +34,22 @@ public class ProductoRouter
                                 .onErrorResume(throwable -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).build())
                     );
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> listarProductoPaginadoRoute(ListarProductosPaginadoUseCaseImp useCase)
+    {
+        return route(GET("/productos/listar/{pagina}/{cantPorPagina}"),
+                     request -> {
+                                    Integer pagina       = Integer.valueOf(request.pathVariable("pagina"));
+                                    Integer tamanoPagina = Integer.valueOf(request.pathVariable("cantPorPagina"));
+
+                                    return ServerResponse.ok()
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .body(BodyInserters.fromPublisher(useCase.apply(pagina, tamanoPagina), ProductoDTO.class))
+                                            .onErrorResume(throwable -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+                                }
+                    );
+    }
+
 
 }
---
+++ src/main/java/epa/InventoryApp/usecase/producto/ListarProductosPaginadoUseCaseImp.java
@@ -0,0 +1,46 @@
+package epa.InventoryApp.usecase.producto;
+
+import epa.InventoryApp.drivenAdapters.bus.RabbitMqPublisher;
+import epa.InventoryApp.drivenAdapters.repository.ProductoRepository;
+import epa.InventoryApp.models.Producto;
+import epa.InventoryApp.models.dto.ProductoDTO;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class ListarProductosPaginadoUseCaseImp implements ListarProductosPaginadoUseCase
+{
+    //------------------------------------------------------------------------- (Inyeccion de Dependencias)
+    private ProductoRepository repositorio;
+    private RabbitMqPublisher eventBus;
+
+    public ListarProductosPaginadoUseCaseImp(ProductoRepository repositorio, RabbitMqPublisher eventBus)
+    {
+        this.repositorio = repositorio;
+        this.eventBus    = eventBus;
+    }
+
+    //------------------------------------------------------------------------- (Implementación Uso de Caso)
+    @Override
+    public Flux<ProductoDTO> apply(int pagina, int tamanoPagina)
+    {
+        return repositorio.findAll()
+                .skip((pagina - 1) * tamanoPagina)
+                .take(tamanoPagina)
+                .map(this::getProductoDTO);
+    }
+
+    //------------------------------------------------------------------------- (Casteo)
+    private ProductoDTO getProductoDTO(Producto productoModel)
+    {
+        return new ProductoDTO(productoModel.getId(),
+                               productoModel.getDescripcion(),
+                               productoModel.getExistencia(),
+                               productoModel.getCosto(),
+                               productoModel.getPrecioUnitarioAlDetalle(),
+                               productoModel.getPrecioUnitarioAlPorMayor(),
+                               productoModel.getUnidadesMinimaAlPorMayor());
+    }
+
+
+}
